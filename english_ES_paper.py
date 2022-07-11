992d955f9e147f4af1c36e91d52210f652f94b8c
David Dong <david0911.dong@gmail.com>
2022-07-11 19:45:56 +0800

    usage: python english_ES_paper.py 8

--- english_ES_paper.py
+++ english_ES_paper.py
@@ -1,22 +1,46 @@
 import turtle as t
+import sys
 
 t.speed('fastest')
 t.bgcolor('white')
-t.pencolor('light grey')
+t.pencolor('grey')
 t.pensize(1)
+y_start = 440
+x_start = -758
 
-def draw_line(y, y_step):
+def draw_line(x, y, y_step):
     for a in range(0, 6):
         t.penup()
-        t.goto(-700, y)
+        t.goto(x, y)
         t.pendown()
-        t.goto(700, y)
+        t.goto(0 - x, y)
         y = y - y_step
-        t.penup()
 
-y_start = 440
-y_step = 15
-for a in range(0, 9):
-    draw_line(y_start, y_step)
-    y_start = y_start - (y_step * 6 + 10)
-   
+def draw_angle(x, x_step, y, y_step):
+    for a in range(0, 35):
+        t.penup()
+        t.goto(x, y)
+        t.pendown()
+        t.goto(x - y_step * 5, y - y_step * 5)
+        x = x + x_step
+
+
+print(len(sys.argv)) # argument to indicate the line count
+
+if len(sys.argv) == 2:
+    line_count = int(sys.argv[1])
+else:
+    line_count = 8
+
+y_step = 2 * y_start / (line_count * 7) 
+x_step = y_step * 3
+
+for a in range(0, line_count):
+    draw_line(x_start, y_start, y_step)
+    draw_angle(x_start, x_step, y_start, y_step)
+    y_start = y_start - (y_step * 7)
+
+t.hideturtle()
+
+    
+    
